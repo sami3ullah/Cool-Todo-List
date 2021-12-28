169cce7d445c39a567d72bd93cd7a1a47e1fe6df
Samiullah <msami398@gmail.com>
2021-12-28 21:26:29 +0500

    feature: input field now adds the items dyanamically & also styled the items

--- index.js
+++ index.js
@@ -15,19 +15,23 @@ function addToDo(event) {
   todoDiv.classList += "todo";
   // create LI
   const newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
+  newTodo.innerText = todoInput.value;
   newTodo.classList += "todo-item";
   todoDiv.appendChild(newTodo);
   // check mark button
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList += "complete-btn";
   todoDiv.appendChild(completedButton);
   // trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList += "complete-btn";
+  trashButton.classList += "trash-btn";
   todoDiv.appendChild(trashButton);
-
   // attach it to the ul
   todoList.appendChild(todoDiv);
+
+  // resetting the input
+  todoInput.value = "";
+  todoInput.focus();
 }
