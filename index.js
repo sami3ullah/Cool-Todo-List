a6d2774cc99f211ff54382274c03382fd4aa7b09
Samiullah <msami398@gmail.com>
2021-12-28 21:09:45 +0500

    Add button now adds a pre-defined list with checkmark and delete button

---
+++ index.js
@@ -0,0 +1,33 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+todoButton.addEventListener("click", addToDo);
+
+// functions
+function addToDo(event) {
+  // prevents form from submitting
+  event.preventDefault();
+  //   todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList += "todo";
+  // create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList += "todo-item";
+  todoDiv.appendChild(newTodo);
+  // check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  todoDiv.appendChild(completedButton);
+  // trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList += "complete-btn";
+  todoDiv.appendChild(trashButton);
+
+  // attach it to the ul
+  todoList.appendChild(todoDiv);
+}
